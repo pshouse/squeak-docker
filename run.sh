a094842d82fb62e737daf7bd3c4013f2e8288f29
Patrick Shouse <shouse.patrick@gmail.com>
2014-12-18 18:14:42 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-service nginx start &
 Xvfb -screen 0 1024x768x16 -ac &
 sleep 1
-x11vnc -forever -usepw -display :0 &
-websockify -D 8080 :5900  &
 DISPLAY=:0 $IMAGEROOT/Contents/Linux-i686/bin/squeak -vm-sound-null $RESOURCES/Squeak*.image /_run.st
