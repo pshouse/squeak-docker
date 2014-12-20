4602c8631c0cfd1c14b605f873d46b683759e938
pshouse <shouse.patrick@gmail.com>
2014-12-20 01:54:05 +0000

    Evaluate

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 Xvfb -screen 0 1024x768x16 -ac &
 sleep 1
-DISPLAY=:0 $IMAGEROOT/Contents/Linux-i686/bin/squeak -vm-sound-null $RESOURCES/Squeak*.image /_run.st
+DISPLAY=:0 $IMAGEROOT/Contents/LinuxAndWindows/Linux-i686/bin/squeak -vm-sound-null $RESOURCES/Squeak*.image /_run.st $1 
