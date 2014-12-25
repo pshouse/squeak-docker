311da3dec0119ac5cce7c6c116f1ec9798ac92d8
Patrick Shouse <shouse.patrick@gmail.com>
2014-12-24 22:26:40 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-Xvfb -screen 0 1024x768x16 -ac &
-sleep 1
-DISPLAY=:0 $IMAGEROOT/Contents/LinuxAndWindows/Linux-i686/bin/squeak -vm-sound-null $RESOURCES/Squeak*.image /_run.st $1 
+#Xvfb -screen 0 1024x768x16 -ac &
+#sleep 1
+#DISPLAY=:0 
+$IMAGEROOT/Contents/LinuxAndWindows/Linux-i686/bin/squeak -vm-sound-null --headless $RESOURCES/Squeak*.image /_run.st $1 
